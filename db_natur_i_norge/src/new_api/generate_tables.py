c396237d2a61822aea7f005c142088fbff6d4a15
Peter Horvath <peter.horvath@geo.uio.no>
2022-06-09 12:56:23 +0200

    update labels

--- db_natur_i_norge/src/new_api/generate_tables.py
+++ db_natur_i_norge/src/new_api/generate_tables.py
@@ -3,13 +3,16 @@ from new_api.table_generator import generate_minor_type_table, \
     generate_major_type_table, \
     generate_major_type_group_table, \
     api
+# %%
+import warnings
+warnings.filterwarnings("ignore") #ADB server has some certificate issues, temporarily suppress
 
 versions = api.get_versions()
 version = versions[-1]
-major_type_groups_table = generate_major_type_group_table(version)
-major_type_table = generate_major_type_table(major_type_groups_table, version)
+major_type_groups_table, _ = generate_major_type_group_table(version)
+major_type_table, _ = generate_major_type_table(major_type_groups_table, version)
 minor_type_table = generate_minor_type_table(major_type_table, version, 5000)
 minor_type_table = generate_minor_type_table(major_type_table, version, 20000)
 
 
-# %%
+
